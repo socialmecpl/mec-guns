8a9b3088bd0a43d396eb64f1a7a18d4963df69f7
Devavrat <39828560+socialmecpl@users.noreply.github.com>
2019-09-24 16:48:59 +0530

    implemeted firebase utils, included ability to store athenitcated users into our firestore database..

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Signin from '../../components/sign-in/sigin-in.component';
+import SignUp from '../../components/signup/signup.component';
 
 import './signin-and-signup.styles.scss';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <Signin />
+        <SignUp />
     </div>
 );
 
