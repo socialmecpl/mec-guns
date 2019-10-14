320ef3ae509220a2495040b60ed4a89acafdb7c4
Devavrat <39828560+socialmecpl@users.noreply.github.com>
2019-10-14 17:50:51 +0530

    some impoovements added

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,24 @@
 import {createSelector} from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    arc: 1,
+    plasma: 2,
+    hvof: 3,
+    flame: 4,
+    cold: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+export const selectCollection = collectionUrlParam => 
+createSelector(
+    [selectCollections],
+    collections => 
+        collections.find(
+            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 )
