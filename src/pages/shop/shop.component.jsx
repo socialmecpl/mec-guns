077f0dcf8f571f8f6abbd216c266b3a475942b80
Devavrat <39828560+socialmecpl@users.noreply.github.com>
2019-10-14 16:02:12 +0530

    shop component added into the redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    
 
-    render() {
-        const {collections} = this.state;
+import {selectCollections} from '../../redux/shop/shop.selector';
+const ShopPage = ({collections}) => {
         return (
             <div className='shop-page'>
                 {
@@ -22,7 +14,11 @@ class ShopPage extends React.Component {
                 }
             </div>
         )
-    }
 } 
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+
+export default connect(mapStateToProps)(ShopPage);
---
+++ src/redux/shop/shop.data.js
@@ -0,0 +1,170 @@
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'Arc',
+      routeName: 'arc',
+      items: [
+        {
+          id: 1,
+          name: 'ARCJET 96 ®',
+          imageUrl: 'https://www.mecpl.com/images/arcjet-96-gun.jpg',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'ARCJET 99ED',
+          imageUrl: 'https://www.mecpl.com/images/arcjet-99ed.jpg',
+          price: 18
+        },
+        {
+          id: 3,
+          name: 'ARCJET Power Source',
+          imageUrl: 'https://www.mecpl.com/images/arcjet-power-source.jpg',
+          price: 35
+        },
+        {
+          id: 4,
+          name: 'ARCJET-101/700 System',
+          imageUrl: 'https://www.mecpl.com/images/arcjet-101-700-sys.jpg',
+          price: 25
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: 'Plasma',
+      routeName: 'plasma',
+      items: [
+        {
+          id: 10,
+          name: 'MF-4MB',
+          imageUrl: 'https://www.mecpl.com/images/plasma-spray-gun-01.jpg',
+          price: 220
+        },
+        {
+          id: 11,
+          name: '9MBM',
+          imageUrl: 'https://www.mecpl.com/images/plasma-spray-gun-02.jpg',
+          price: 280
+        },
+        {
+          id: 12,
+          name: 'MSG 100',
+          imageUrl: 'https://www.mecpl.com/images/plasma-spray-gun-03.jpg',
+          price: 110
+        },
+        {
+          id: 13,
+          name: 'MINI ID GUN',
+          imageUrl: 'https://www.mecpl.com/images/plasma-spray-gun-04.jpg',
+          price: 160
+        },
+        {
+          id: 14,
+          name: 'MASS FLOW "VIEW" AIR PLASMA SPRAY SYSTEM',
+          imageUrl: 'https://www.mecpl.com/images/plasma-spray-system-01.jpg',
+          price: 160
+        },
+        {
+          id: 15,
+          name: 'MASS FLOW PLASMA SYSTEM',
+          imageUrl: 'https://www.mecpl.com/images/mass-flow-plasma-system.jpg',
+          price: 160
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'HVOF',
+      routeName: 'hvof',
+      items: [
+        {
+          id: 18,
+          name: 'HIPOJET® 2700',
+          imageUrl: 'https://www.mecpl.com/images/hvof-powder-spray-system.jpg',
+          price: 125
+        },
+        {
+          id: 19,
+          name: 'Hybrid Attachment for denser coating',
+          imageUrl: 'https://www.mecpl.com/images/hybrid-attachment.jpg',
+          price: 90
+        },
+        {
+          id: 20,
+          name: 'XPOJET® 5000',
+          imageUrl: 'https://www.mecpl.com/images/xpojet-5000-installation.jpg',
+          price: 90
+        },
+        {
+          id: 21,
+          name: 'MJP-6000™ GUN',
+          imageUrl: 'https://www.mecpl.com/images/mjp-6000.jpg',
+          price: 165
+        },
+        {
+          id: 22,
+          name: 'HIJET® 9610',
+          imageUrl: 'https://www.mecpl.com/images/hijet-9610-spray-system.jpg',
+          price: 185
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Flame',
+      routeName: 'flame',
+      items: [
+        {
+          id: 23,
+          name: 'Powderjet® 86',
+          imageUrl: 'https://www.mecpl.com/images/powderjet-86.jpg',
+          price: 25
+        },
+        {
+          id: 24,
+          name: 'POWDERJET® 85',
+          imageUrl: 'https://www.mecpl.com/images/powderjet-85.jpg',
+          price: 20
+        },
+        {
+          id: 25,
+          name: 'RODOJET® 9810',
+          imageUrl: 'https://www.mecpl.com/images/rodojet-9810-gun.jpg',
+          price: 80
+        },
+        {
+          id: 26,
+          name: 'SPRAYJET® 88',
+          imageUrl: 'https://www.mecpl.com/images/sprayjet-88-gun.jpg',
+          price: 80
+        },
+        {
+          id: 27,
+          name: 'SPRAYJET® 98r',
+          imageUrl: 'https://www.mecpl.com/images/sprayjet-98-gun.jpg',
+          price: 45
+        },
+        {
+          id: 28,
+          name: 'SPRAYJET® EL',
+          imageUrl: 'https://www.mecpl.com/images/sprayjet-el-gun.jpg',
+          price: 135
+        },
+      ]
+    },
+    {
+      id: 5,
+      title: 'Cold',
+      routeName: 'cold',
+      items: [
+        {
+          id: 30,
+          name: 'DYMET Cold Spray Equipment Model - 423 (Made in Russia)',
+          imageUrl: 'https://www.mecpl.com/images/dymet-model-423.jpg',
+          price: 325
+        }
+      ]
+    }
+  ];
+  export default SHOP_DATA;
